7a276b5e80d7b114e754d24dd690fa7b56b231dd
Wyatt Kirby <wyatt@apsis.io>
2017-03-05 13:15:49 -0800

    Update to allow exclude key

--- test/test.js
+++ test/test.js
@@ -9,9 +9,12 @@ var orca   = require('../dist/orca.js');
 describe('Orca', function() {
   var app, callbacks, callbackOne, callbackTwo;
 
-  beforeEach(function() {
-    app         = orca.default; // reset instance on each test
+  before(function() {
+    app = orca.default; // reset instance on each test
+  });
 
+  beforeEach(function() {
+    app.reset();
     callbacks   = [];
     callbackOne = simple.spy(function() {});
     callbackTwo = simple.spy(function() {});
@@ -20,26 +23,54 @@ describe('Orca', function() {
   describe('#registerAction', function() {
     it('should register actions', function() {
       app.registerGlobalAction(callbackOne);
-      callbacks = app._callbacks[app._globalKey][app._entryKey];
-      assert.ok(_.includes(_.flattenDeep(callbacks), callbackOne));
+      callbacks = _.flattenDeep(app._callbacks[app._globalKey][app._entryKey]);
+      assert.ok(_.some(callbacks, ['func', callbackOne]));
     });
 
     it('should register actions with priority', function() {
-      app.registerGlobalAction(callbackOne, 99);
-      app.registerGlobalAction(callbackTwo, 100);
+      app.registerGlobalAction(callbackOne, {priority: 99});
+      app.registerGlobalAction(callbackTwo, {priority: 100});
 
       callbacks = _.flattenDeep(app._callbacks[app._globalKey][app._entryKey][99]);
 
-      assert.ok(_.includes(callbacks, callbackOne));
-      assert.equal(false, _.includes(callbacks, callbackTwo));
+      assert.ok(_.some(callbacks, ['func', callbackOne]));
+      assert.equal(false, _.some(callbacks, ['func', callbackTwo]));
+    });
+
+    it('should reset', function() {
+      app.registerGlobalAction(callbackOne);
+      callbacks = _.flattenDeep(app._callbacks[app._globalKey][app._entryKey]);
+      assert.ok(_.some(callbacks, ['func', callbackOne]));
+
+      app.reset();
+      assert.ok(_.isEmpty(app._callbacks));
     });
   });
 
   describe('#run', function() {
     it('should run all callbacks in the global scope', function() {
       app.registerGlobalAction(callbackOne);
-      app.run();
+      app.run(app._globalKey);
 
+      assert.equal(1, callbackOne.callCount);
+    });
+
+    it('should only run globals once when calling global scope', function() {
+        app.registerGlobalAction(callbackOne);
+        app.run();
+
+        assert.equal(1, callbackOne.callCount);
+    });
+
+    it('should not run callbacks in excluded scopes', function() {
+      app.registerGlobalAction(callbackOne, {excludes: ['foo']});
+
+      // Runs in global scope
+      app.run();
+      assert.equal(1, callbackOne.callCount);
+
+      // Does not run in foo scope
+      app.run('foo');
       assert.equal(1, callbackOne.callCount);
     });
 
@@ -74,8 +105,8 @@ describe('Orca', function() {
     });
 
     it('should call higher priority callbacks first', function() {
-      app.registerGlobalAction(callbackOne, 10);
-      app.registerGlobalAction(callbackTwo, 20);
+      app.registerGlobalAction(callbackOne, {priority: 10});
+      app.registerGlobalAction(callbackTwo, {priority: 20});
 
       app.run();
 
